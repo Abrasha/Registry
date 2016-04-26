b619fff453cd8e3b9ffde06a0e1438940a0ea186
Michael Khmelovsky <hmelmin@gmail.com>
2016-04-26 21:01:38 +0300

    Integer instead of int and blindly coded tests. full version

--- src/main/java/edu/kpi/dao/TownDAO.java
+++ src/main/java/edu/kpi/dao/TownDAO.java
@@ -13,5 +13,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface TownDAO extends JpaRepository<Town, Integer>{
-    
+
 }
--- src/main/java/edu/kpi/model/PersonType.java
+++ src/main/java/edu/kpi/model/PersonType.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 @Entity
 public class PersonType {
 
-    private int id;
+    private Integer id;
     private String typeName;
 
     public PersonType() {
@@ -23,11 +23,11 @@ public class PersonType {
 
     @Id
     @GeneratedValue
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
