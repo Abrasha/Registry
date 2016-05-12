a4cc5b5cefc8ca8f0f852a648cb7ceb24eaf30d7
Michael Khmelovsky <hmelmin@gmail.com>
2016-05-12 14:13:13 +0300

    Selenium Google test

--- src/test/java/edu/kpi/SeleniumTest.java
+++ src/test/java/edu/kpi/SeleniumTest.java
@@ -1,22 +1,19 @@
 package edu.kpi;
 
-import junit.framework.TestCase;
-import junit.framework.Assert;
-import junit.Test;
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
 
-public class SimpleTest{
+//import junit.Test;
 
+public class SeleniumTest{
     @Test
     public void setUp() throws Exception {
-       WebDriver driver = new  FirefoxDriver();
-        driver.navigate().to("http://seleniumsimplified.com");
-        Assert.assertTrue("title should start differently",
-                            driver.getTitle().StartsWith("Selenium Simplified"));
-        driver.close();
+        WebDriver driver = new  FirefoxDriver();
+        driver.get("http://www.google.com");
+        Assert.assertEquals("Google", driver.getTitle());
+
     }
 
 
